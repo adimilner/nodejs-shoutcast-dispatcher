3954930a5990ec23d8566b7d01e2bba1b4f9429b
Kevin Holly <root@dedilink.eu>
2012-11-03 00:41:32 +0100

    Bugfixes and standby-mode added

--- shoutcast-dispatcher.js
+++ shoutcast-dispatcher.js
@@ -4,54 +4,67 @@ var url          = "http://dsl.tb-stream.net:80/"
   , statsint     = 15000
   , statsheadint = 20
   , listenip     = "0.0.0.0"
-  , listenport   = 8000;
+  , listenport   = 8001
+  , strmidletime = 5000;
 //CONFIG END
 
-// INIT START
+// BUILD START
 var http          = require("http")
   , radio         = require("radio-stream")
-  , slots         = {}
   , statsstate    = 0
   , bytessent     = 0
   , bytesreceived = 0
-  , stream        = radio.createReadStream(url)
-  , slot          = null
+  , stream        = null
   , server        = null
-  , slotsused     = null;
-// INIT END
+  , stopstreamtck = null;
+// BUILD END
 
-stream.on("data", function (chunk) {
-	bytesreceived += chunk.length;
-	for (slot in slots){
-		slots[slot].write(chunk);
-		bytessent += chunk.length;
-	};
-});
-
-server = http.createServer(function(req, res){
+server = http.createServer(function(req, res) {
 	res.writeHead(200,{
 		"Content-Type":		"audio/mpeg",
 		"Transfer-Encoding":	"chunked",
 		"Connection":		"Close"
 	});
-	if(server.connections <= maxslots)
-	{
-		var time = new Date().getTime();
-		slots[time] = res;
-		console.error("Connected user: "+time);
+	if(server.connections <= maxslots) {
+		console.log("Client connected");
+		if(stream == null || stream.connected != true || stopstreamtck != null) {
+			if(stopstreamtck != null) {
+				console.log("Reusing stream connection");
+				clearTimeout(stopstreamtck);
+				if(stopstreamtck == null) {
+					stream = radio.createReadStream(url);
+				}
+				stopstreamtck = null;
+			}
+			else {
+				console.log("Building stream connection ...");
+				stream = radio.createReadStream(url);
+			}
+		}
+		stream.on("data", function(chunk) {
+			res.write(chunk);
+			bytesreceived += chunk.length/server.connections;
+			bytessent += chunk.length;
+		});
 		res.on("close", function() {
-			delete slots[time];
-			console.error("Disconnected user: "+time);
+			console.log("Client disconnected.");
+			if(server.connections == 0) {
+				console.log("Last client disconnected. Killing stream connection ...");
+				stopstreamtck = setTimeout(function() {
+					stream.destroy();
+					stream = null;
+					stopstreamtck = null;
+					console.log("Stream connection killed.");
+				}, strmidletime);
+			}
 		});
 	}
 	else
 		res.end("Server full");
 });
-
 setInterval(function() {
 	slotsused = server.connections;
-	if(statsheadint != 0)
-	{
+	if(statsheadint != 0) {
 		if(statsstate == 0)
 			console.log("STATSDESC:SLOTSUSED|BYTESRECEIVED|BYTESSENT");
 		statsstate += 1;
@@ -60,5 +73,4 @@ setInterval(function() {
 	}
 	console.log("STATS:"+slotsused+"|"+bytesreceived+"|"+bytessent);
 }, statsint);
-
 server.listen(listenport, listenip);
